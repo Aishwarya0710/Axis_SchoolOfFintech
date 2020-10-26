c0c073a49b260fde709da2be7a6d6c36d5f72977
Aishwarya0710 <48089276+Aishwarya0710@users.noreply.github.com>
2020-10-26 23:09:48 +0530

    Add files via upload

---
+++ Online Pathology Test Booking Application -Kotlin/model/Test.kt
@@ -0,0 +1,9 @@
+package com.manipal.model
+class Test {
+    var testId: Int = 0
+    var testName: String = ""
+    var labName: String = ""
+    var labLocation: String = ""
+    var price: Int = 0
+
+}
